92f53719935bd642187ea10f83537b193a435126
Harsh Mathur <harshmathur2002@gmail.com>
2021-09-08 14:30:17 +0530

    Adding cors options

--- server/server.js
+++ server/server.js
@@ -17,7 +17,29 @@ const app = express()
 
 //MIDDLEWARE
 
-app.use(cors())
+const whitelist = ["http://localhost:3000"]
+
+const corsOptions = {
+
+  origin: function (origin, callback) {
+
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+
+      callback(null, true)
+
+    } else {
+
+      callback(new Error("Not allowed by CORS"))
+
+    }
+
+  },
+
+  credentials: true,
+
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json({ limit: "1mb" }));
 
